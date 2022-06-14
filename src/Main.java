c587f5ed04cb1f3338aedecad38ca34882b4bee1
Terrell Mabry <tmabry@xpanxion.com>
2022-06-14 16:08:11 -0400

    finished project

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");}}
+    public static void main (String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter number of weeks");
+        int number = input.nextInt();
+        int day = number *7;
+        System.out.println(day+"days");
+        int hours = day *24;
+        System.out.println(hours+ "hours");
+        int minutes = hours *60;
+        System.out.println(minutes+ "minutes");
+        int seconds = minutes *60;
+        System.out.println(seconds+ "seconds");
+    }
+
+    }
+
